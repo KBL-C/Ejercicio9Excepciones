3d8f018240d5941b2059ae65ce6f02a8a2299c57
kumar <rjbaltarlopezcozar@gmail.com>
2020-02-03 20:05:57 +0100

    otro commit

--- src/ejer9_rangoexception/Ejer9_RangoException.java
+++ src/ejer9_rangoexception/Ejer9_RangoException.java
@@ -5,8 +5,10 @@
  */
 package ejer9_rangoexception;
 
+import java.util.Scanner;
+
 /**
- *
+ *Esta es la clase principal donde creamos el objeto ALumno y probamso todos los métodos.
  * @author kumar
  */
 public class Ejer9_RangoException {
@@ -15,7 +17,22 @@ public class Ejer9_RangoException {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+            Scanner sci = new Scanner(System.in);
+            Scanner scd = new Scanner(System.in);
+            Alumno alumno = new Alumno();          
+            alumno.setNombre("Kumar");
+            System.out.println(alumno.getNombre());
+            alumno.pedirNota();
+            System.out.println("Modificar la nota");
+            System.out.println("introduce la posición y la nota para poner en esa posición");
+            int pos = sci.nextInt();
+            double not = scd.nextDouble();
+            alumno.modificarNota(pos, not);
+            alumno.imprimirCalificaciones();
+            
+            System.out.println(alumno.toString());
     }
     
 }
