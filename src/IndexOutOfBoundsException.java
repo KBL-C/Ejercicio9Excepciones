c5eb98ebc3049486b050b7af528722cb82b8f5e3
kumar <rjbaltarlopezcozar@gmail.com>
2020-02-03 20:01:15 +0100

    excepción IndexOutOsBoundsxception creada

---
+++ src/IndexOutOfBoundsException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *Esta es la clase con la excepción para la posición fuera del arrayList
+ * @author kumar
+ */
+public class IndexOutOfBoundsException extends Exception{
+    public IndexOutOfBoundsException(String message){
+        super(message);
+    }
+    
+    
+}
