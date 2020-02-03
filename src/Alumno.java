516d868aaf083eb1a20a752f12962d6b1229b7cb
kumar <rjbaltarlopezcozar@gmail.com>
2020-02-03 19:49:13 +0100

    declaración de variables de la clase alumno

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,9 +8,16 @@
  */
 
 /**
- *
+ *Esta es la clase alumno que representa un objeto de tipo alumno
  * @author kumar
  */
 public class Alumno {
+    /**
+     * Aquí declaramos las variables
+     */
+    final static int numAsignaturas = 5;
+    private String nombre;
+    ArrayList<Double> notas = new ArrayList();
+    
     
 }
