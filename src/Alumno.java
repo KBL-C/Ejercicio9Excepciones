1743e5cca93e2a122e84ada393d7c211d3f56e31
kumar <rjbaltarlopezcozar@gmail.com>
2020-02-03 19:51:36 +0100

    método pedir nota creado

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -49,6 +50,36 @@ public class Alumno {
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    
+    /**
+     * método void que pide una nota y lo almacena en el arrayList.
+     * @throws RangoException es la excepción que salta en caso de que la nota que introducamos no esta válida, 
+     * es decir que sea menor que 0 ó mayor que 10.
+     * para detectar el error utilizamso el try{}catch, el tray si detecta el error lo envia la catch donde lo redirigimos con la excepción y mostramos un mensaje
+     * indicando el tipo de error(excepción).
+     */
+    public void pedirNota() throws RangoException {
+        Scanner scd = new Scanner(System.in);
+        double nota;
+        try{
+            Alumno alumno = new Alumno();
+            for (int j = 0; j < numAsignaturas; j++) {
+                System.out.println("Introduce la nota del alumno");
+                nota = scd.nextDouble();              
+                if (nota <0 || nota > 10){
+                    throw new RangoException("Error, nota no válida");//aquí le estamos indicando que lance la excepción en caso de error
+                }
+                
+                notas.add(nota);//añade la nota al array list
+                
+            }
+            
+        
+        }catch(RangoException ex){
+                System.out.println(ex.getMessage()); //este es el catch que recoge el error 
+        }
+        
     }
 
     
