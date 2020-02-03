17be6c88404b205d21c7269ce779b61d7b9bfc1a
kumar <rjbaltarlopezcozar@gmail.com>
2020-02-03 19:46:10 +0100

    RangoException creada

---
+++ src/RangoException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *Esta clase es una excepción que salta cuando el valor esta fuera del rango.
+ * @author kumar
+ */
+public class RangoException extends Exception{
+    public RangoException(String message){
+        super(message);
+    }
+    
+    
+}
